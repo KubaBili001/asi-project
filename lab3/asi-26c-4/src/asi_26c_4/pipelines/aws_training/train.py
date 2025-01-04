13079c44197511d5c0b20526c01d1c3a41addafe
MaksDeb <s25001@pjwstk.edu.pl>
2025-01-04 18:50:56 +0100

    added training with amazon sagemaker

---
+++ lab3/asi-26c-4/src/asi_26c_4/pipelines/aws_training/train.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import joblib
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--test-size', type=float, default=0.2, help="Proporcja zbioru testowego")
+    parser.add_argument('--random-state', type=int, default=42, help="Losowy seed dla podziału danych")
+
+    parser.add_argument('--output-data-dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR'))
+    parser.add_argument('--model-dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
+    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
+    parser.add_argument('--test', type=str, default=os.environ.get('SM_CHANNEL_TEST'))
+
+    args, _ = parser.parse_known_args()
+
+    print(f"Wczytywanie danych z: {args.train}")
+    train_data_path = os.path.join(args.train, "verified_employees.pq")
+    data = pd.read_parquet(train_data_path)
+
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+
+    print(f"Podział danych: test_size={args.test_size}, random_state={args.random_state}")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args.test_size, random_state=args.random_state)
+
+    print("Rozpoczęcie treningu modelu...")
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"Mean Squared Error (MSE) na zbiorze testowym: {mse}")
+
+    print(f"Zapis modelu do katalogu: {args.model_dir}")
+    model_path = os.path.join(args.model_dir, "model.joblib")
+    joblib.dump(model, model_path)
+
+    print(f"Model zapisany w: {model_path}")
